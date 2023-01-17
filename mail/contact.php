11bd4b90fa1947ece6371a4a29be1f0126a5e9e7
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-17 14:29:38 -0300

    ok

--- mail/contact.php
+++ mail/contact.php
@@ -73,7 +73,19 @@ if ($mail->addReplyTo($_POST['email'], 'bootstrapfriendly')) {
 $mail->Subject = 'Contact Request from bootstrapfriendly demo form';
 //Keep it simple - don't use HTML
 $mail->isHTML(true);
+?>
 //Build a simple message body
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<?php
 $mail->Body = "
 
 
@@ -88,7 +100,7 @@ $mail->Body = "
         <td style='padding:8px;border:1px solid #ccc;'> {$_POST['email']} </td>
     </tr>
     <tr>
-        <td style='padding:8px; border:1px solid #ccc;'>Contact No.:</td>
+        <td style='padding:8px; border:1px solid #ccc;'>Subject:</td>
         <td style='padding:8px;border:1px solid #ccc;'> {$_POST['subject']} </td>
     </tr>
     <tr>
@@ -120,3 +132,11 @@ echo "<b class='text-danger'>Invalid email address, message ignored.</b>";
 } else {
 echo "<b class='text-danger'>Sorry, something went wrong. Please try again later.</b>";
 }
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+</script>
+</body>
+
+</html>
