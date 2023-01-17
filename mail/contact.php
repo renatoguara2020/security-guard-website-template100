cdeb387b68191ec425c2dd5ebbe1f531c7aa487d
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-17 11:49:06 -0300

    ok

--- mail/contact.php
+++ mail/contact.php
@@ -46,7 +46,7 @@ $mail->isSMTP(); //Send using SMTP
 $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
 $mail->SMTPAuth = true; //Enable SMTP authentication
 $mail->Username = 'renatoguara2020@gmail.com'; //SMTP username
-$mail->Password = 'agciqpqrqferdazp'; // password is optional //SMTP password
+$mail->Password = 'agc'; // password is optional //SMTP password
 // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port = 587;
 $mail->CharSet = 'UTF-8';
